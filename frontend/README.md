3ad323234e051ff18a61f04582e9aa75a043244c
Ali Tout <alitout95@gmail.com>
2024-10-08 14:24:21 +0300

    Project Start

---
+++ frontend/README.md
@@ -0,0 +1,5 @@
+to use this blank project you have to:
+1- npm i
+2- npm i -D sass
+3- npm i bootstrap
+4- npm i axios 
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './style/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
