de2be81c99be6a1caac1a58d1f5071ebff310f03
Ali Tout <alitout95@gmail.com>
2024-12-10 13:50:15 +0200

    Fixed Navbar Responsiveness

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import Menu01 from '@untitled-ui/icons-react/build/cjs/Menu01';
 
+const NavLinks = () => (
+    <>
+        <a href='#' className='navLink text-decoration-none'>
+            Home
+        </a>
+        <a href='#' className='navLink text-decoration-none'>
+            Programs
+        </a>
+        <a href='#' className='navLink text-decoration-none'>
+            Master Classes
+        </a>
+        <a href='#' className='navLink text-decoration-none'>
+            Community
+        </a>
+    </>
+);
 
 function Navbar() {
-
     const navigate = useNavigate();
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -19,22 +34,8 @@ function Navbar() {
                 <div className="logo">
                     <Logo />
                 </div>
-                <div className="navLinks d-none d-lg-flex gap-3 gap-lg-5 fs-1_125 fw-600 ">
-                    <a href='#' className='navLink text-decoration-none text-gray-900'>
-                        Home
-                    </a>
-                    <a href='#' className='navLink text-decoration-none text-gray-900'>
-                        Programs
-                    </a>
-                    <a href='#' className='navLink text-decoration-none text-gray-900'>
-                        Master Classes
-                    </a>
-                    <a href='#' className='navLink text-decoration-none text-gray-900'>
-                        Community
-                    </a>
-                    <a href='#' className='navLink text-decoration-none text-gray-900'>
-                        Academy Coffee Space
-                    </a>
+                <div className="navLinks d-none d-lg-flex gap-3 gap-lg-5 fs-1_125 fw-600">
+                    <NavLinks />
                 </div>
                 <div className="mobile-menu-button d-lg-none" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                     <Menu01 />
@@ -42,28 +43,13 @@ function Navbar() {
                 <div className="offcanvas offcanvas-top" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     <div className="offcanvas-body">
                         <div className="d-flex flex-column align-items-center text-center gap-3 fs-1_125 fw-600">
-                            <a href='#' className='navLink text-decoration-none text-gray-900'>
-                                Home
-                            </a>
-                            <a href='#' className='navLink text-decoration-none text-gray-900'>
-                                Programs
-                            </a>
-                            <a href='#' className='navLink text-decoration-none text-gray-900'>
-                                Master Classes
-                            </a>
-                            <a href='#' className='navLink text-decoration-none text-gray-900'>
-                                Community
-                            </a>
-                            <a href='#' className='navLink text-decoration-none text-gray-900'>
-                                Academy Coffee Space
-                            </a>
+                            <NavLinks />
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-    )
+    );
 }
 
 export default Navbar;
